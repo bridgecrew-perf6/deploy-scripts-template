c3320060c8ad24a0dd4b11af3219effde5f86267
fujia <fujia.site@outlook.com>
2021-12-28 16:45:27 +0800

    fix: fixed build-docker.sh issues

--- template/build-docker.sh
+++ template/build-docker.sh
@@ -9,7 +9,7 @@ IMAGE_ID=$(docker image ls -q <%= mirrorName %>:<%= mirrorVersion %>)
 # 3. use free container mirror service and login private repository，recommended:
 #       - aliyun(https://help.aliyun.com/document_detail/257112.html?spm=5176.166170.J_5253785160.5.93cf5164mGxRDG)
 #       - tencent(https://console.cloud.tencent.com/tcr)
-docker login --owner=<%= owner %> --password=<%= userPwd %> <%= repoZone %>
+docker login --username=<%= owner %> --password=<%= userPwd %> <%= repoZone %>
 
 docker tag $IMAGE_ID <%= repoZone %>/<%= repoNamespace %>/<%= mirrorName %>:<%= mirrorVersion %>
 
